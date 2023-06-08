b1c2b91387ff8b164ae07d33efad4f1268267d3a
michael kwon <tailofmoon@naver.com>
2023-06-08 21:54:47 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,5 +1,13 @@
 // main.js
 $(function(){
-    console.log(111)
+    console.log(111);
+    $('header nav .depth1 > li').hover(
+        function(){
+            // 마우스 올라갔을 때
+        },
+        function(){
+            //  마우스 나갔을 때
+        }
+    );
 
 });
